ba888909a269d9d41456d9f9ee5e9e842f2eef09
shikha105 <shikha105789@gmail.com>
2024-04-04 15:26:01 -0700

    added status codes to the responses and made all the http responses uniform with the help of Response Entity

--- src/main/java/com/portfolio/firstjobapp/job/JobController.java
+++ src/main/java/com/portfolio/firstjobapp/job/JobController.java
@@ -1,5 +1,7 @@
 package com.portfolio.firstjobapp.job;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +17,23 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll() {
+
+        return  ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job) {
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id) {
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+
         Job job = jobService.getJobById(id);
         if(job!=null)
-          return job;
-        return new Job(1L, "testtitle", "testDescription", "1000", "40000", "van");
+          return new ResponseEntity<>(job,HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-}
+;}
