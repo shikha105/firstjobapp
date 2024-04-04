0fd918dad4a9e22daa2837e7caa9ebe128d08d90
shikha105 <shikha105789@gmail.com>
2024-04-04 09:22:21 -0700

    create job method is implemented.

--- src/main/java/com/portfolio/firstjobapp/job/JobController.java
+++ src/main/java/com/portfolio/firstjobapp/job/JobController.java
@@ -1,16 +1,25 @@
 package com.portfolio.firstjobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class JobController {
 
-private List<Job> jobs = new ArrayList<>();
+    private List<Job> jobs = new ArrayList<>();
 
-     @GetMapping
-    public List<Job> findAll(){
+    @GetMapping("/jobs")
+    public List<Job> findAll() {
         return jobs;
+    }
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "Job added successfully";
     }
 }
