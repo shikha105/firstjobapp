fe9412904d948ae60f3734f13ae2680281fa09ca
shikha105 <shikha105789@gmail.com>
2024-04-04 08:46:49 -0700

    Added Job class and Job Controller

--- src/main/java/com/portfolio/firstjobapp/job/JobController.java
+++ src/main/java/com/portfolio/firstjobapp/job/JobController.java
@@ -1,4 +1,16 @@
 package com.portfolio.firstjobapp.job;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JobController {
+
+private List<Job> jobs = new ArrayList<>();
+
+     @GetMapping
+    public List<Job> findAll(){
+        return jobs;
+    }
 }
