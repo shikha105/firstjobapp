9f395fa8028046f9981f12424e1fe597b37b5886
shikha105 <shikha105789@gmail.com>
2024-04-04 10:03:25 -0700

    Job Service class and the implementation is added, changes are made accordingly in the job controller class

---
+++ src/main/java/com/portfolio/firstjobapp/job/JobService.java
@@ -0,0 +1,10 @@
+package com.portfolio.firstjobapp.job;
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+
+    void createJob(Job job);
+}
---
+++ src/main/java/com/portfolio/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,22 @@
+package com.portfolio.firstjobapp.job.impl;
+
+import com.portfolio.firstjobapp.job.Job;
+import com.portfolio.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+    private List<Job> jobs = new ArrayList<>();
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+     jobs.add(job);
+    }
+}
